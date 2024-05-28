8744fae8e41878eb3b563162ab51cdf1f2afa286
Nitin Dixit <nitinsdixit03@gmail.com>
2024-05-28 13:33:30 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,19 @@
+let a="abc";
+let b = 5;
+console.log(a+b);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof (a+b));
+
+const a1 = {
+    name: "nitin",
+    age: 19,
+    iscricketer: false,
+}
+console.log(a1);
+console.log(a1.age);
+
+a1["friend"] =`ballu`;
+
+console.log(a1);
