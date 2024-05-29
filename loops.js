0a3163023baf8d28c9297645a51381ebcda26f4d
Nitin Dixit <nitinsdixit03@gmail.com>
2024-05-29 13:20:31 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,43 @@
+//FOR LOOP
+
+// let sum = 0;
+// for(let i=0;i<4;i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// FOR IN LOOP
+
+// let obj = {
+//     nitin: 82,
+//     ronak: 92,
+//     ballu: 95,
+// }
+
+// for(let a in obj){
+//     console.log("marks of "+ a + " is " + obj[a])
+// }
+
+// FOR OF LOOP
+
+// for(let a of "ronak"){
+//     console.log(a);
+// }
+
+// WHILE LOOP
+
+// let n = 10;
+// let i= 0;
+// while(i<=n){
+//     i++;
+//     console.log(i);
+// }
+
+// do while loop
+
+let n= 10;
+let i= 5;
+do{
+    console.log(i);
+    i++;
+}while(i>n)
