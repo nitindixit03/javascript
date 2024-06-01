853742113b75fe6cd5dd4aaf2951230aa516c332
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-01 21:34:15 +0530

    dom

---
+++ DOM/script.js
@@ -0,0 +1,26 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
+
+let ctitle = document.querySelectorAll(".card-title")
+ctitle[0].style.color = "red";
+ctitle[1].style.color = "blue";
+ctitle[2].style.color = "green";
+
+document.querySelector(".this").style.color = "cyan";
