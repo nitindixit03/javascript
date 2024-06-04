ec04ee35f8a6ecb15e596f7beb442a8eb6946b71
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-04 18:53:06 +0530

    setTimeout and setInterval

---
+++ setTimeout.js
@@ -0,0 +1,35 @@
+// document.write("hello");
+
+
+// let b = prompt("do you want to use setTimeout");
+
+
+// let a = setTimeout(function(){
+//     alert("hey watts up!!");
+// },5000);
+
+// if(b=="no"){
+//     clearTimeout(a);
+// }
+
+// to stop setTimeot use clearTimeout(a);
+
+// const sum  = (a,b)=>{
+//     alert("yes i am running " + (a+b) );
+// }
+
+// setTimeout(sum, 5000, 10,10)
+
+
+// setInterval(function (){
+//     alert("hello world!!");
+
+// },3000);
+
+// use clearInterval to stop this
+
+
+const sum1 = (x = 4, y = 6) => {
+    alert("sum of x and y " +  (x + y));
+  }
+  setInterval(sum1, 3000)
