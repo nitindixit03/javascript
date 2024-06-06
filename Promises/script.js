202b7baa5fbe3e3e11790342f4670620e379abb9
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-07 02:50:52 +0530

    promise chaning

--- Promises/script.js
+++ Promises/script.js
@@ -16,28 +16,55 @@
 
 
 
-let p1 = new Promise((resolve,reject) => {
-    console.log("promise is pending");
+// let p1 = new Promise((resolve,reject) => {
+//     console.log("promise is pending");
+//     setTimeout(()=>{
+//         // console.log("I am a Promise and I am fulfilled");
+//         resolve(true);
+//     },5000)
+// });
+
+// let p2 = new Promise((resolve,reject) => {
+//     console.log("promise is pending");
+//     setTimeout(()=>{
+//         // console.log("I am a Promise and I am rejected");
+//         reject(new Error("i am an error"));
+//     },5000)
+// });
+
+// // console.log(p1,p2);
+
+// p1.then((value)=>{
+//     console.log(value);
+// });
+
+// p2.catch((error)=>{
+//     console.log("some error in p2")
+// })
+
+
+//PROMISE CHANING
+
+
+let p1 = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        // console.log("I am a Promise and I am fulfilled");
-        resolve(true);
-    },5000)
-});
+        console.log("resolved after 2 seconds")
+    },2000)
+    resolve(56);
+})
 
-let p2 = new Promise((resolve,reject) => {
-    console.log("promise is pending");
-    setTimeout(()=>{
-        // console.log("I am a Promise and I am rejected");
-        reject(new Error("i am an error"));
-    },5000)
-});
-
-// console.log(p1,p2);
-
-p1.then((value)=>{
+p1.then((value) => {
     console.log(value);
-});
 
-p2.catch((error)=>{
-    console.log("some error in p2")
+    let p2 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise-2")
+        },2000)
+    })
+    return p2;
+}).then((value)=>{
+    console.log(value);
+    return 2;
+}).then((value)=>{
+    console.log("finally done!!");
 })
