05cc33d07e12612c2813e8cc240893d939dc0ee5
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-07 20:22:35 +0530

    promise

--- Promises/script.js
+++ Promises/script.js
@@ -86,3 +86,57 @@
 // p1.then(()=>{
 //     console.log("hurray!!")
 // });
+
+// PROMISE API
+
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("value-1");
+    },1000)
+})
+
+let p2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("value-2");
+        // reject(new Error("error"));
+    },2000)
+})
+
+let p3 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("value-3");
+    },3000)
+})
+
+// p1.then((value)=>{
+//     console.log(p1);
+// })
+
+// p2.then((value)=>{
+//     console.log(p2);
+// })
+
+// p3.then((value)=>{
+//     console.log(p3);
+// })
+
+// make all these promise so that it all come at a same time
+
+let promise_all = Promise.all([p1,p2,p3])   // it will give an array after 3 seconds
+                                            // but if any one of them having error then it won't work. but if we want to make we can use Promise.allSettled()
+
+promise_all.then((value)=>{
+    console.log(value);
+})
+
+let promise_al = Promise.allSettled([p1,p2,p3])   // it will give an array after 3 seconds
+                                           
+
+promise_al.then((value)=>{
+    console.log(value);
+})
+
+//  Promise.race(promise) give only one value which resolve first  or reject first it will give that value only!! 
+// Promise.any(promise)  (which comes first)give only one value that value will be resolve  but if error comes it will move to next one but if  all are carring error it will show an Aggregate Error.
+// Promise.resolve(value) give resolve value
+// Promise.reject(error) give reject value
