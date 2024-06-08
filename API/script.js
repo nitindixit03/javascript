14e868bffca0a3106dc5b9f80bf1130cf63e568c
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-08 17:28:15 +0530

    fetch

---
+++ API/script.js
@@ -0,0 +1,26 @@
+
+
+const createTodo = async()=>{
+let options = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        title: 'nitin',
+        body: 'bhai',
+        userId: 1111,
+    }),
+}
+
+let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+let response = await  p.json();
+return response;
+}
+
+const mainFunc = async()=>{
+    let todo = await createTodo();
+    console.log(todo);
+}
+mainFunc();
+
