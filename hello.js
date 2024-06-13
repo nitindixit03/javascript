b69d4f5259a45e296c5f771ed8a5c5a149c7bdfa
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-14 00:22:52 +0530

    change

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+// console.log("Hello, world!");
+
+for(let i=0;i<4;i++){
+    setTimeout(()=>{
+        console.log(i);
+    },10)
+}
+
+for(var i=0;i<4;i++){
+        setTimeout(()=>{
+            console.log(i);
+        },10)
+    }
+
+    // both will have different output
